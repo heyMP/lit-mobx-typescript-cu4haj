42e2098605cca3ceb20fce8c419ed2310266059a
heyMP <mgp140@gmail.com>
2021-12-06 14:54:56 -0500

    2 files modified

--- index.ts
+++ index.ts
@@ -1,38 +1,16 @@
-import { html, TemplateResult } from 'lit';
-import { customElement, LitElement } from 'lit/decorators.js';
-import { observable, action } from 'mobx';
-import { MobxLitElement } from '@adobe/lit-mobx';
+import { LitElement, html, css } from 'lit';
+import { MobxReactionUpdate } from '@adobe/lit-mobx';
+import { store } from './store';
 
-// create a mobx observable
-class Counter {
-  @observable
-  public count = 0;
-
-  @action
-  public increment() {
-    this.count++;
-  }
-}
-
-// create instance that can be shared across components
-const counter = new Counter();
-
-// create a new custom element, and use the base MobxLitElement class
-// alternatively you can use the MobxReactionUpdate mixin, e.g. `class MyElement extends MobxReactionUpdate(LitElement)`
-@customElement('my-element')
-class MyElement extends MobxLitElement {
-  private counter = counter;
-
+export class MyElement extends MobxReactionUpdate(LitElement) {
   // any observables accessed in the render method will now trigger an update
-  public render(): TemplateResult {
+  public render() {
     return html`
-            Count is ${this.counter.count}
-            <br />
-            <button @click=${this.incrementCount}>Add</button>
-        `;
-  }
-
-  private incrementCount() {
-    this.counter.increment();
+          Count is ${store.count}
+          <br />
+          <button @click=${() => store.increment()}>Add</button>
+      `;
   }
 }
+
+customElements.define('my-element', MyElement);
